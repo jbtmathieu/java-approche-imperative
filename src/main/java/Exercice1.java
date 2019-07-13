671f9ff0ca1884cd187df9743d23939a78b4f3d3
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-13 20:48:48 +0200

    //Refactoring

--- src/main/java/Exercice1.java
+++ src/main/java/Exercice1.java
@@ -1,8 +1,7 @@
-
+//TP05 - Approche Imperative
 public class Exercice1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ; 
 		
@@ -14,7 +13,7 @@ public class Exercice1 {
 			System.out.print(array[i]+" ");
 		}
 		
-		//Passage à la ligne
+		//Passage ï¿½ la ligne
 		System.out.println();
 		
 		/* Ordre Inverse */
@@ -24,7 +23,7 @@ public class Exercice1 {
 		}
 		System.out.println();
 		
-		/* entiers supérieurs à trois */
+		/* entiers supï¿½rieurs ï¿½ trois */
 		System.out.println("Entiers >3 :");
 		for (int i=0;i<array.length;i++) {
 			if(array[i]>3) {
@@ -62,8 +61,8 @@ public class Exercice1 {
 		}
 		System.out.println();
 		
-		/* Le plus grand élément */
-		System.out.println("Le plus grand élément est :");
+		/* Le plus grand ï¿½lï¿½ment */
+		System.out.println("Le plus grand ï¿½lï¿½ment est :");
 		
 		int valeurMax=array[0];
 		for (int i=0;i<array.length;i++) {
@@ -74,8 +73,8 @@ public class Exercice1 {
 		System.out.println(valeurMax);
 		
 		
-		/* Le plus petit élément */
-		System.out.println("Le plus petit élément est :");
+		/* Le plus petit ï¿½lï¿½ment */
+		System.out.println("Le plus petit ï¿½lï¿½ment est :");
 		
 		int valeurMin=array[0];
 		for (int i=0;i<array.length;i++) {
---
+++ src/main/java/Exercice10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercice10 {
+
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		double[] array= new double[10];
+		double[] array2 = new double[10];
+		System.out.println("Menu: 1 pour ajouter 2 pour afficher:");
+		int choix=0;
+		
+		switch (choix) {
+		case 1: 
+			//if 
+			break;
+		case 2:
+			for (int i=0; i<array2.length;i++) {
+				System.out.println(array2[i]);
+			}
+			break;
+			default:
+				;
+		}
+		
+		
+	}
+
+}
